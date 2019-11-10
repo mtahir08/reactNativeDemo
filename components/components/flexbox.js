70b0178a710944c1fce7f97db78ff9b814ef43c6
Muhammad Tahir <mtahir.ps@gmail.com>
2019-11-10 23:31:38 +0500

    flex box

--- components/components/flexbox.js
+++ components/components/flexbox.js
@@ -16,24 +16,46 @@ const FlexBox = () => {
             </View>
         </View>
     )
+    // return (
+
+    //     <View style={styles.container}>
+    //         <View style={styles.box}>
+    //             <Text style={styles.text}>1</Text>
+    //         </View>
+    //         <View style={styles.middleBox}>
+    //             <Text style={styles.text}>2</Text>
+    //         </View>
+    //         <View style={styles.box}>
+    //             <Text style={styles.text}>3</Text>
+    //         </View>
+    //     </View>
+    // )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'baseline',
         flex: 1,
-
+        backgroundColor: '#D4E6F1',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     box: {
-        backgroundColor: "#5E777C",
-        // flex: 1,
-        borderWidth: StyleSheet.hairlineWidth,
-        margin: 5,
+        backgroundColor: '#5E777C',
+        padding: 20,
+        flex: 1,
+        margin: 5
+    },
+    middleBox: {
+        backgroundColor: '#f4f4f4',
+        flex: 4,
+        padding: 20,
 
+        alignSelf: 'center',
+        margin: 5
     },
     text: {
+        fontSize: 30,
         color: '#273032',
         textAlign: 'center',
         padding: 10
