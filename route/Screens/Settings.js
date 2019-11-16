b85f5beca50b7f13cc1834880670ab60bbe6ee02
Muhammad Tahir <mtahir.ps@gmail.com>
2019-11-16 23:30:10 +0500

    files add

---
+++ route/Screens/Settings.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { rootStyles } from './Styles'
+export default function App() {
+    return (
+        <View style={rootStyles.container}>
+            <Text style={rootStyles.text}>First</Text>
+        </View>
+    );
+}
---
+++ route/constants/Colors.js
@@ -0,0 +1,13 @@
+const tintColor = '#2f95dc';
+export default {
+    tintColor: tintColor,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColor,
+    tabBar: '#fefefe',
+    errorBackground: 'red',
+    errorText: '#fff',
+    warningBackground: '#EAEB5E',
+    warningText: '#666804',
+    noticeBackground: tintColor,
+    noticeText: '#fff',
+};
