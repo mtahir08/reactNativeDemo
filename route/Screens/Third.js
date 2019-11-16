82dafb9a3622b6d7e041c86f0424cd7dc3042960
Muhammad Tahir <mtahir.ps@gmail.com>
2019-11-16 23:29:26 +0500

    add screen

---
+++ route/Screens/Third.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { rootStyles } from './Styles'
+export default function App() {
+    return (
+        <View style={rootStyles.container}>
+            <Text style={rootStyles.text}>Third</Text>
+        </View>
+    );
+}
