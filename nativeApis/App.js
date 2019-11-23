45e70412465527459e97188660592cc6bc98bec5
Muhammad Tahir <mtahir.ps@gmail.com>
2019-11-23 12:32:06 +0500

    react native cli

---
+++ nativeApis/App.js
@@ -0,0 +1,73 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+  Image,
+  Dimensions,
+} from 'react-native';
+
+import ImaagePicker from './ImagePicker';
+import Phone from './Phone';
+
+const App: () => React$Node = () => {
+  const [component, setComponent] = useState(null);
+
+  const showComponents = () => {
+    switch (component) {
+      case 'imagePicker':
+        return <ImaagePicker />;
+      case 'dial':
+        return <Phone />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        {component ? (
+          showComponents()
+        ) : (
+          <>
+            <Button
+              onPress={() => {
+                setComponent('imagePicker');
+              }}
+              title="Image Picker"
+            />
+            <Button
+              onPress={() => {
+                setComponent('dial');
+              }}
+              title="Open Dial"
+            />
+          </>
+        )}
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height - 30,
+  },
+});
+
+export default App;
