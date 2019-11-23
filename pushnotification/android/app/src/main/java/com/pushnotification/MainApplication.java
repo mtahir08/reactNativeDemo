0c9578944961e8bca34f04983a8427340f3d0235
Muhammad Tahir <mtahir.ps@gmail.com>
2019-11-24 04:39:28 +0500

    configuration

--- pushnotification/android/app/src/main/java/com/pushnotification/MainApplication.java
+++ pushnotification/android/app/src/main/java/com/pushnotification/MainApplication.java
@@ -1,5 +1,4 @@
 package com.pushnotification;
-
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -8,6 +7,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -24,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new RNFirebaseMessagingPackage());
           return packages;
         }
 
